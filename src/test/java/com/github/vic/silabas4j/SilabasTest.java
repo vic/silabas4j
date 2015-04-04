d661fea300ab17e31224343bce3ee30e051f7365
Victor Borja <vborja@apache.org>
2015-04-04 02:56:07 -0600

    tests

--- src/test/java/com/github/vic/silabas4j/SilabasTest.java
+++ src/test/java/com/github/vic/silabas4j/SilabasTest.java
@@ -3,33 +3,63 @@ package com.github.vic.silabas4j;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.yaml.snakeyaml.Yaml;
 
 import static org.junit.Assert.*;
 
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+@RunWith(Parameterized.class)
 public class SilabasTest {
 
-    private static Map<String, Map<String, Object>> testCases;
+    private static final String YAML_RESOURCE = "silabas-test.yaml";
+    private static final String[] WORD_EXPECTATIONS = {
+            "syllables"
+    };
 
-    @BeforeClass
-    public static void setUp() throws Exception {
+    private Silabas subject;
+
+    @Parameterized.Parameter(value = 0)
+    public String word;
+
+    @Parameterized.Parameter(value = 1)
+    public Collection<String> expectedSyllables;
+
+
+    @Parameterized.Parameters(name = "{0}")
+    public static Collection<Object[]> data() throws Exception {
         Yaml yaml = new Yaml();
-        InputStream resourceAsStream = SilabasTest.class.getClassLoader().getResourceAsStream("silabas-test.yaml");
-        testCases = yaml.loadAs(resourceAsStream, Map.class);
+        InputStream resourceAsStream = SilabasTest.class.getClassLoader().getResourceAsStream(YAML_RESOURCE);
+        Map<String,Map<String,Object>> testCases = yaml.loadAs(resourceAsStream, Map.class);
+        Object[] objects = testCases.entrySet().stream().map(SilabasTest::entryData).toArray();
+        return Arrays.<Object[]>asList(objects);
     }
 
+    private static Object[] entryData(Map.Entry<String, Map<String,Object>> entry) {
+        Stream word = Stream.of(entry.getKey());
+        Map<String, Object> map = entry.getValue();
+        Stream expectations = Arrays.stream(WORD_EXPECTATIONS).map(map::get);
+        return Stream.concat(word, expectations).toArray();
+    }
+
+    @Before
+    public void before() {
+       this.subject = Silabas.process(word);
+    }
 
     @Test
-    public void testSilabas() throws Exception {
-        testCases.forEach((String word, Map asserts)-> {
-            Silabas silabas = Silabas.process(word);
-            Object[] expected = ((ArrayList) asserts.get("syllables")).toArray();
-            Object[] actual = silabas.getSyllables().toArray();
-            assertArrayEquals("Syllables for: "+word, expected, actual);
-        });
+    public void testSyllables() throws Exception {
+        Collection<CharSequence> syllables = subject.getSyllables();
+        assertArrayEquals("Syllables for: "+word, expectedSyllables.toArray(), syllables.toArray());
     }
+
 }
